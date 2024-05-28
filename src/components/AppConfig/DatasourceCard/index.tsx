257e58a05e76e4b382f0debc29af101d65982efa
andreatrio <andrea99cla@gmail.com>
2024-05-28 18:08:14 +0200

    init datasource card

---
+++ src/components/AppConfig/DatasourceCard/index.tsx
@@ -0,0 +1,17 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+
+interface DatasourceCardInterface {
+
+}
+
+export default function DatasourceCard({
+
+}: DatasourceCardInterface) {
+  
+  return (
+    <Stack>
+
+    </Stack>
+  )
+}
